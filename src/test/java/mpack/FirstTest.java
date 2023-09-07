e92c0626886e5db6b1f969a63cf57ad6ca34ab3d
Fethiye <fethiye0623@gmail.com>
2023-09-07 10:33:59 +0200

    new class added..

--- src/test/java/mpack/FirstTest.java
+++ src/test/java/mpack/FirstTest.java
@@ -1,23 +1,21 @@
 package mpack;
 
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
+import org.junit.Assert;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
-
-import java.util.concurrent.TimeUnit;
-
+import org.openqa.selenium.chrome.ChromeOptions;
 public class FirstTest {
     @Test
-    public void mOne() {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://www.amazon.de/");
-        driver.close();
+    public void OpenBrowser()  {
+        WebDriver driver;
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        options.addArguments("disable-gpu");
+        driver = new ChromeDriver(options);
+        driver.get("https://www.google.com");
+        System.out.println("Title of the page is: " + driver.getTitle());
+        Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
     }
 }
-
