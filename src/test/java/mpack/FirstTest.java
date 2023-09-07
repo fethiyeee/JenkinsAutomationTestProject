6a4fde559793a6963246a2307d19da4e458b14e0
Fethiye <fethiye0623@gmail.com>
2023-09-07 10:13:15 +0200

    new class added..

---
+++ src/test/java/mpack/FirstTest.java
@@ -0,0 +1,23 @@
+package mpack;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class FirstTest {
+    @Test
+    public void mOne() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://www.amazon.de/");
+        driver.close();
+    }
+}
+
